e300b19e373bb7b21c352ce85ef93a6876300fbd
Mandar Kunte <kunte.mandar@gmail.com>
2019-03-19 15:45:25 -0700

    Update Stringpermutations.js

--- Stringpermutations.js
+++ Stringpermutations.js
@@ -4,22 +4,19 @@ function permMain(arr) {
 	perm(arr.split(''),0)
 }
 function perm(arr, i) {
-
 	if(i === arr.length-1) { 
   	console.log(arr.join(''))
-  	return
  	}
   for(var j=i; j<arr.length;j++){
-  	let sa = swap(arr,i,j)
-    perm(sa, i+1)
-    swap(sa,i,j)
+  	swap(arr,i,j)
+    perm(arr, i+1)
+    swap(arr,i,j)
   }
 }
 function swap(input, index_A, index_B) {
   var temp = input[index_A];
   input[index_A] = input[index_B];
   input[index_B] = temp;
-	return input
 }
 permMain('abc')
 
