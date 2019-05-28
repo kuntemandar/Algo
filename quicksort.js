07de46170b015032d5b641694d5b454589a05da3
Mandar Kunte <kunte.mandar@gmail.com>
2019-05-28 11:51:42 -0700

    Update quicksort.js

--- quicksort.js
+++ quicksort.js
@@ -1,36 +1,34 @@
-function qs(arr,l,r){
-	if(arr.length < 2)
-  	return arr
-  
-  let p = partition(arr,l,r)
-  if(l <= p-1)
-  	qs(arr,l,p-1)
-  if(r>=p+1)
-  	qs(arr,p+1,r)
-	return arr
-}
-function swap(a,i,j){
-let t = a[i]
-a[i] = a[j]
-a[j] = t
-}
-function partition(arr,l,r){
-	var p   = Math.floor((r + l) / 2) //middle 
-  swap(arr,p,0)
-	while(l <= r){
-  	while(arr[l] <= arr[0])
-    	l++
-    while(arr[r] > arr[0])
-    	r--
-    if(l<=r){
-    	swap(arr,l,r)
-      l++
-      r--
-    } 
+function quickSort(arr, left, right){
+   var len = arr.length, 
+   pivot,
+   partitionIndex;
+
+
+  if(left < right){
+    pivot = right;
+    partitionIndex = partition(arr, pivot, left, right);
+    
+   //sort left and right
+   quickSort(arr, left, partitionIndex - 1);
+   quickSort(arr, partitionIndex + 1, right);
   }
-	swap(arr,0,r)
-  return r
+  return arr;
 }
-let arr = [1,3,5,6,2,99,-7]
-let r = arr.length-1
-console.log(qs(arr,0,r),'sorted array')
+function partition(arr, pivot, left, right){
+   var pivotValue = arr[pivot],
+       partitionIndex = left;
+
+   for(var i = left; i < right; i++){
+    if(arr[i] < pivotValue){
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(arr, right, partitionIndex);
+  return partitionIndex;
+}
+function swap(arr, i, j){
+   var temp = arr[i];
+   arr[i] = arr[j];
+   arr[j] = temp;
+}
